bd80a7bf3c7468cc4df742b4aca52924b3e1e7a1
RubenJ99 <juarezperezruben13@gmail.com>
2022-03-20 23:45:29 +0100

    putisimo ajax

--- app/controllers/StoreHouseController.js
+++ app/controllers/StoreHouseController.js
@@ -24,222 +24,92 @@ export default class StoreHouseController {
    */
   #preLoadStoreHouseData() {
     //BLOQUE GENERACION DE PRODUCTOS
-    let clothingItem1;
-    let clothingItem2;
-    let clothingItem3;
+   
+    let cats;
+    let prods;
+    let stores;
 
-    try {
-      clothingItem1 = new Clothes(
-        111,
-        "Blue Hoodie",
-        "A blue hoodie",
-        30,
-        21,
-        ["../../public/media/bluehoodie.jpg"],
-        "XL",
-        "Blue",
-        "M"
-      );
-      clothingItem2 = new Clothes(
-        222,
-        "Red Skirt",
-        "A red skirt",
-        27,
-        21,
-        ["../../public/media/redskirt.jpg"],
-        "S",
-        "Red",
-        "F"
-      );
-      clothingItem3 = new Clothes(
-        333,
-        "Black Trousers",
-        "A pair of black trousers",
-        40,
-        21,
-        ["../../public/media/blacktrousers.jpg"],
-        "M",
-        "Black",
-        "U"
-      );
-    } catch (error) {
-      console.error(error);
-    }
+    let rq = new Request("../../database/model/data.json");
 
-    let perfumeItem1;
-    let perfumeItem2;
-    let perfumeItem3;
+    //ASYNC FETCH
 
-    try {
-      perfumeItem1 = new Perfume(
-        444,
-        "CK One",
-        "Perfume from calvin klein",
-        49.95,
-        21,
-        ["../../public/media/ckone.jpg"],
-        "flowers/citric",
-        "F"
-      );
-      perfumeItem2 = new Perfume(
-        555,
-        "Eau de Rochas",
-        "Perfume from Rochas",
-        54.0,
-        21,
-        ["../../public/media/eaurochas.png"],
-        "citric",
-        "F"
-      );
-      perfumeItem3 = new Perfume(
-        999,
-        "Acqua di Gioia",
-        "Perfume from Giorgio Armani",
-        53.9,
-        21,
-        ["../../public/media/acquadigioia.jpg"],
-        "flowers/fresh",
-        "F"
-      );
-    } catch (error) {
-      console.error(error);
-    }
 
-    let smartWatchItem1;
-    let smartWatchItem2;
-    let smartWatchItem3;
 
-    try {
-      smartWatchItem1 = new SmartWatch(
-        666,
-        "Apple Watch",
-        "New apple watch model 12",
-        800,
-        21,
-        ["../../public/media/applewatch.jpg"],
-        "AW-12",
-        "Square",
-        "White"
-      );
-      smartWatchItem2 = new SmartWatch(
-        777,
-        "Samsung Watch",
-        "New samsung watch s8",
-        650,
-        21,
-        ["../../public/media/samsungwatch.jpg"],
-        "SMGW-S8",
-        "Round",
-        "White"
-      );
-      smartWatchItem3 = new SmartWatch(
-        888,
-        "Xiaomi Mi band 7",
-        "New mi band 7",
-        30,
-        21,
-        ["../../public/media/miband.png"],
-        "XMI7",
-        "Stretched",
-        "Black"
-      );
-    } catch (error) {
-      console.error(error);
-    }
+    console.log('a');
 
-    //BLOQUE GENERACION DE CATEGORIAS
-    let cat1;
-    let cat2;
-    let cat3;
-    try {
-      cat1 = new Category(
-        "Bottom-Clothing",
-        "Clothes that fit under the waist"
-      );
-      cat2 = new Category("Props", "");
-      cat3 = new Category("Perfume-and-Cologne", "");
-    } catch (error) {
-      console.error(error);
-    }
 
-    //BLOQUE GENERACION DE TIENDAS
-    let store1;
-    let store2;
-    let store3;
-    try {
-      store1 = new Store(
-        "S2447905G",
-        "Tienda Madrid",
-        "C/ Mayor 12",
-        967899011,
-        new Coords("50.4536", "30.5164 50° 27′ 13″ North, 30° 30′ 59″ East"),
-        "mmStore.jpg"
-      );
-      store2 = new Store(
-        "H50463439",
-        "Tienda Ciudad Real",
-        "C/ Menor 21",
-        957899011,
-        new Coords("50.4536", "30.5164 50° 27′ 13″ North, 30° 30′ 59″ East"),
-        "primorStore.jpg"
-      );
-      store3 = new Store(
-        "P0636988H",
-        "Tienda Barcelona",
-        "C/ Ramblas 2",
-        997899011,
-        new Coords("50.4536", "30.5164 50° 27′ 13″ North, 30° 30′ 59″ East"),
-        "zaraStore.png"
-      );
-    } catch (error) {
-      console.error(error);
-    }
 
-    try {
-      this.#storeHouseModel.addCategory(cat1);
-      this.#storeHouseModel.addCategory(cat2);
-      this.#storeHouseModel.addCategory(cat3);
-    } catch (error) {
-      console.error(error);
-    }
 
-    try {
-      this.#storeHouseModel.addProduct(clothingItem2, cat1);
-      this.#storeHouseModel.addProduct(clothingItem3, cat1);
 
-      this.#storeHouseModel.addProduct(smartWatchItem1, cat2);
-      this.#storeHouseModel.addProduct(smartWatchItem2, cat2);
-      this.#storeHouseModel.addProduct(smartWatchItem3, cat2);
+    $.ajax({
+        url: "../../database/model/data.json",
+        method: 'GET',
+        async: false,
+      }).done((data)=> {
+        cats = data.categories;
+        cats.forEach(elem => {
+            let tmp = new Category(elem.title,elem.description);
+            
+            this.#storeHouseModel.addCategory(tmp);
+        });
 
-      this.#storeHouseModel.addProduct(perfumeItem1, cat3);
-      this.#storeHouseModel.addProduct(perfumeItem2, cat3);
-      this.#storeHouseModel.addProduct(perfumeItem3, cat3);
-    } catch (error) {
-      console.error(error);
-    }
+        stores = data.stores;
+        stores.forEach(elem => {
+            let tmp = new Store(elem.cif,elem.name,elem.address,elem.phone,new Coords(elem.latitude,elem.longitude),elem.img);
+            this.#storeHouseModel.addShop(tmp);
+        });
 
-    try {
-      this.#storeHouseModel.addShop(store1);
-      this.#storeHouseModel.addShop(store2);
-      this.#storeHouseModel.addShop(store3);
-    } catch (error) {
-      console.error(error);
-    }
+      }).fail(function(){
+          
+      });
+        
+   
 
-    try {
-      this.#storeHouseModel.addProductInShop(clothingItem2, store1);
-      this.#storeHouseModel.addProductInShop(clothingItem3, store1);
 
-      this.#storeHouseModel.addProductInShop(perfumeItem1, store2);
+    // try {
+    //   this.#storeHouseModel.addCategory(cat1);
+    //   this.#storeHouseModel.addCategory(cat2);
+    //   this.#storeHouseModel.addCategory(cat3);
+    // } catch (error) {
+    //   console.error(error);
+    // }
 
-      this.#storeHouseModel.addProductInShop(perfumeItem2, store3);
-      this.#storeHouseModel.addProductInShop(smartWatchItem1, store3);
-      this.#storeHouseModel.addProductInShop(smartWatchItem2, store3);
-    } catch (error) {
-      console.error(error);
-    }
+    // try {
+    //   this.#storeHouseModel.addProduct(clothingItem2, cat1);
+    //   this.#storeHouseModel.addProduct(clothingItem3, cat1);
 
-    //TODO esto peta porque fatatas, error en el modelo
+    //   this.#storeHouseModel.addProduct(smartWatchItem1, cat2);
+    //   this.#storeHouseModel.addProduct(smartWatchItem2, cat2);
+    //   this.#storeHouseModel.addProduct(smartWatchItem3, cat2);
+
+    //   this.#storeHouseModel.addProduct(perfumeItem1, cat3);
+    //   this.#storeHouseModel.addProduct(perfumeItem2, cat3);
+    //   this.#storeHouseModel.addProduct(perfumeItem3, cat3);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
+    // try {
+    //   this.#storeHouseModel.addShop(store1);
+    //   this.#storeHouseModel.addShop(store2);
+    //   this.#storeHouseModel.addShop(store3);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
+    // try {
+    //   this.#storeHouseModel.addProductInShop(clothingItem2, store1);
+    //   this.#storeHouseModel.addProductInShop(clothingItem3, store1);
+
+    //   this.#storeHouseModel.addProductInShop(perfumeItem1, store2);
+
+    //   this.#storeHouseModel.addProductInShop(perfumeItem2, store3);
+    //   this.#storeHouseModel.addProductInShop(smartWatchItem1, store3);
+    //   this.#storeHouseModel.addProductInShop(smartWatchItem2, store3);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+
     // try {
     //     this.#storeHouseModel.addProductInShop(clothingItem1,store1,3);
     //     this.#storeHouseModel.addQuantityProductInShop(clothingItem3,store1,20);
@@ -302,6 +172,7 @@ export default class StoreHouseController {
    * Con esto cargamos los datos en el momento en el que se inicializa la aplicacion
    */
   onLoad = () => {
+
     this.#preLoadStoreHouseData();
 
     let cName = getCookie("user");
@@ -315,6 +186,7 @@ export default class StoreHouseController {
       $("#bModalIni").css("display", "block");
       $("#bCloseS").css("display", "none");
     }
+    
   };
   /**
    * Dado el titulo de la categoria encontramos la que corresponde y almacenamos la categoria completa,luego retornamos el generador que nos da acceso a los productos
